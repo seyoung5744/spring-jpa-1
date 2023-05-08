1f31684ab772efb5db1dfdcba38eeea0d7adbf47
seyoung5744 <seyoung574458@gmail.com>
2023-05-08 22:31:11 +0900

    test : 회원 기능 테스트
    - 회원가입을 성공해야 한다.
    - 회원가입 할 때 같은 이름이 있으면 예외가 발생해야 한다

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.service;
+
+import static org.junit.Assert.fail;
+
+import javax.persistence.EntityManager;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+//    @Rollback(false)
+    public void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+//        em.flush();
+        Assertions.assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); // 예외가 발생해야 한다!
+
+//        try {
+//            memberService.join(member2); // 예외가 발생해야 한다!
+//        } catch (IllegalStateException e) {
+//            return;
+//        }
+
+        //then
+        fail("예외가 발생해야 한다.");
+    }
+}
